0b791665ce7a6f74b1ec960f61ea6a86cf0cad22
Leawind <leawind@yeah.net>
2025-07-01 18:50:55 +0800

    update static-resource

--- src/static-resource/index.ts
+++ src/static-resource/index.ts
@@ -3,22 +3,44 @@ import * as std_path from '@std/path@1';
 
 /**
  * A class for managing static resources.
+ *
+ * ## Usage
+ *
+ * ```ts
+ * // The first param should always be `import.meta.dirname`
+ * const srm = StaticResourceManager.at(import.meta.dirname!);
+ * ```
+ *
  * It provides methods to fetch various types of static files (e.g., text, JSON, binary data) based on the specified type.
+ *
+ * ## Example
+ *
+ * ```ts
+ * const json = await srm.fetch('file.json');
+ * ```
  */
 export class StaticResourceManager {
-	constructor(
-		/**
-		 * The base directory path where static resources are located.
-		 */
+	protected constructor(
 		private readonly base: string,
-		/**
-		 * Refer to [Window: fetch() method - Web APIs | MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/fetch#options)
-		 */
 		public fetchOptions?: RequestInit,
-	) {}
+	) {
+		this.base = std_path.toFileUrl(base).href;
+	}
 
 	/**
-	 * Fetches a static resource and returns it in the specified format.
+	 * Creates a new StaticResourceManager instance.
+	 *
+	 * @param base - The base directory path where static resources are located.
+	 * @param relativePath - The relative path to the base directory.
+	 * @param fetchOptions - Refer to [Window: fetch() method - Web APIs | MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/fetch#options)
+	 */
+	public static at(base: string, relativePath: string = '.', fetchOptions?: RequestInit): StaticResourceManager {
+		return new StaticResourceManager(std_path.join(base, relativePath), fetchOptions);
+	}
+
+	/**
+	 * Fetch a static resource and return it in the specified format.
+	 *
 	 * If no type is provided, the method infers the type based on the file extension:
 	 * - `.json` files are treated as JSON.
 	 * - Other files are treated as plain text.
@@ -44,11 +66,11 @@ export class StaticResourceManager {
 	public async fetch(specifier: string, type: 'arrayBuffer'): Promise<ArrayBuffer>;
 	public async fetch(specifier: string, type: 'body'): Promise<ReadableStream<Uint8Array> | null>;
 	public async fetch(specifier: string, type: 'formData'): Promise<FormData>;
-	public async fetch(specifier: string, type?: string): Promise<any> {
-		const uri = import.meta.resolve(std_path.join(this.base, specifier));
+	public async fetch(path: string, type?: string): Promise<any> {
+		const uri = std_path.join(this.base, path);
 		const result = await fetch(uri, this.fetchOptions);
 		if (!type) {
-			if (specifier.endsWith('.json')) {
+			if (path.endsWith('.json')) {
 				type = 'json';
 			} else {
 				type = 'text';
--- test/static-resource/index.test.ts
+++ test/static-resource/index.test.ts
@@ -2,10 +2,12 @@ import { StaticResourceManager } from '@/static-resource/index.ts';
 import { assertStrictEquals } from '@std/assert/strict-equals';
 
 Deno.test('Test static resource', async () => {
-	const srm = new StaticResourceManager('../../test/static_resource');
+	const srm = StaticResourceManager.at(import.meta.dirname!);
 
 	assertStrictEquals(await srm.fetch('readme.md'), 'hello\n');
 
 	const steveData = await srm.fetch('data.json');
 	assertStrictEquals(steveData.name, 'Steve');
+
+	assertStrictEquals(await srm.fetch('name.txt'), 'Steve\n');
 });
