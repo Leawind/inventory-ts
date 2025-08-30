e67bc0bc68575b1f513b7ec6892ff04db25fc3f7
Leawind <leawind@yeah.net>
2025-08-30 20:54:14 +0800

    misc
    - Escaper
    - sortObjectkeys

--- src/misc/index.ts
+++ src/misc/index.ts
@@ -1,4 +1,5 @@
 import * as posix_path from '@std/path@1/posix';
+import type { JsonValue } from '@leawind/inventory/types';
 
 export * from '@/misc/zone.ts';
 export * from '@/misc/parsing.ts';
@@ -23,22 +24,81 @@ export class Sets {
 	}
 }
 
-export function escapeMarkers(s: string, markers: string[], esc: string = '\\'): string {
-	s = s.replaceAll(esc, esc + esc);
-	for (let i = 0; i < markers.length; i++) {
-		const marker = markers[i];
-		s = s.replaceAll(marker, `${esc}${marker}`);
-	}
-	return s;
-}
+export class Escaper {
+	private mappings: Map<string, string> = new Map();
 
-export function unescapeMarkers(s: string, markers: string[], esc: string = '\\'): string {
-	for (let i = 0; i < markers.length; i++) {
-		const marker = markers[i];
-		s = s.replaceAll(`${esc}${marker}`, marker);
+	public constructor(
+		public escaper: string = '\\',
+		/**
+		 * marker --> real
+		 *
+		 * ### Example
+		 *
+		 * ```ts
+		 * {
+		 *    n: '\n',
+		 *    t: '\t',
+		 *    r: '\r',
+		 * }
+		 * ```
+		 */
+		mappings?: Record<string, string>,
+	) {
+		this.map(mappings ?? {});
 	}
-	s = s.replaceAll(esc + esc, esc);
-	return s;
+
+	public addDefaultMappings(): this {
+		return this.map({
+			n: '\n',
+			r: '\r',
+			t: '\t',
+			v: '\v',
+			b: '\b',
+			f: '\f',
+			'0': '\0',
+		});
+	}
+
+	public map(mappings: Record<string, string>): this;
+	public map(marker: string, real: string): this;
+	public map(
+		...args:
+			| [mappings: Record<string, string>]
+			| [marker: string, real: string]
+	): this {
+		if (args.length === 1) {
+			this.mappings = new Map(Object.entries(args[0]));
+		} else {
+			const [marker, real] = args;
+			this.mappings.set(marker, real);
+		}
+		return this;
+	}
+
+	public escape(s: string): string {
+		let result = s.replaceAll(this.escaper, this.escaper + this.escaper);
+		for (const [marker, real] of this.mappings) {
+			result = result.replaceAll(real, this.escaper + marker);
+		}
+		return result;
+	}
+	public unescape(realString: string): string {
+		let result = '';
+		let isEscaping = false;
+		for (let i = 0; i < realString.length; i++) {
+			const ch = realString[i];
+			if (isEscaping) {
+				result += this.mappings.get(ch) ?? ch;
+				isEscaping = false;
+			} else if (ch === this.escaper) {
+				isEscaping = true;
+				continue;
+			} else {
+				result += ch;
+			}
+		}
+		return result;
+	}
 }
 
 export function rebasePath(rpath: string, fromBase: string, toBase: string): string {
@@ -94,4 +154,40 @@ export function* range(...args: [number] | [number, number, step?: number]): Ite
 			throw new Error('step must not be zero');
 		}
 	}
+}
+
+/**
+ * Sorts the keys of an object in a consistent order.
+ *
+ * This function works recursively on nested objects and arrays.
+ *
+ * ### Parameters
+ *
+ * - `obj` - The object to sort.
+ * - `compare` - A comparison function for sorting keys. Defaults to lexicographical order.
+ * - `depth` - The depth of nested objects to sort. Defaults to Infinity.
+ *    -  If set to negative, no sorting is applied.
+ *    -  If set to 0, only the top-level keys are sorted.
+ */
+export function sortObjectKeys<T extends JsonValue>(
+	obj: T,
+	compare: (a: string, b: string) => number = (a, b) => a > b ? 1 : a < b ? -1 : 0,
+	depth = Infinity,
+): T {
+	if (depth < 0) {
+		return obj;
+	}
+	if (typeof obj === 'object') {
+		if (obj === null) {
+			return obj;
+		} else if (Array.isArray(obj)) {
+			return obj.map((item) => sortObjectKeys(item, compare, depth - 1)) as T;
+		} else {
+			const entries = Object.entries(obj)
+				.sort((a, b) => compare(a[0], b[0]))
+				.map(([key, value]) => [key, sortObjectKeys(value, compare, depth - 1)]);
+			return Object.fromEntries(entries) as T;
+		}
+	}
+	return obj;
 }
--- src/misc/misc.test.ts
+++ src/misc/misc.test.ts
@@ -1,17 +1,25 @@
-import { assert, assertEquals } from '@std/assert';
-import { escapeMarkers, range, unescapeMarkers } from '@/misc/index.ts';
+import { assertEquals } from '@std/assert';
+import { Escaper, range, sortObjectKeys } from '@/misc/index.ts';
 
-Deno.test('escape', () => {
-	for (
-		const [markers, str, esc] of [
-			[["'"], `I say 'you'!`, `I say \\'you\\'!`],
-			[[], `\\`, `\\\\`],
-			[['"'], `\\"'\\`, `\\\\\\"'\\\\`],
-		] as [string[], string, string][]
-	) {
-		assert(escapeMarkers(str, markers) === esc);
-		assert(unescapeMarkers(esc, markers) === str);
+Deno.test('Escaper', () => {
+	const esc = new Escaper().addDefaultMappings();
+
+	const cases = [
+		['\\', '\\\\'],
+		['\n', '\\n'],
+		['\r', '\\r'],
+		['\t', '\\t'],
+		['\v', '\\v'],
+		['\b', '\\b'],
+		['\f', '\\f'],
+		['\0', '\\0'],
+	];
+	for (const [real, escaped] of cases) {
+		assertEquals(esc.escape(real), escaped, `Escaping ${real}`);
+		assertEquals(esc.unescape(escaped), real, `Unescaping ${escaped}`);
 	}
+
+	assertEquals(esc.unescape('\\q'), 'q');
 });
 
 Deno.test('misc/range', () => {
@@ -23,4 +31,34 @@ Deno.test('misc/range', () => {
 	assertEquals([...range(1, 5, -1)], []);
 	assertEquals([...range(5, 1)], []);
 	assertEquals([...range(5, 1, 1)], []);
+});
+
+Deno.test('misc/sortObjectKeys/basic', () => {
+	const obj = {
+		b: 1,
+		a: 2,
+		c: {
+			f: 9,
+			e: 10,
+			d: 3,
+		},
+	};
+	assertEquals(Object.keys(obj), ['b', 'a', 'c']);
+	const sorted = sortObjectKeys(obj);
+	assertEquals(Object.keys(sorted), ['a', 'b', 'c']);
+	assertEquals(Object.keys(sorted.c), ['d', 'e', 'f']);
+});
+
+Deno.test('misc/sortObjectKeys/consistency', () => {
+	const obj = {
+		b: 1,
+		a: null,
+		c: {
+			f: null,
+			e: 10,
+			d: 3,
+		},
+	};
+	const sorted = sortObjectKeys(obj);
+	assertEquals(obj, sorted);
 });
