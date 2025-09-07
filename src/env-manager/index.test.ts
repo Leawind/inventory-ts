b24401ce192b0186c2729244fbbe72fb95c9966a
Leawind <leawind@yeah.net>
2025-09-07 15:28:23 +0800

    env-manager

---
+++ src/env-manager/index.test.ts
@@ -0,0 +1,40 @@
+import { assertEquals } from '@std/assert/equals';
+import { EnvManager } from './index.ts';
+
+Deno.test('EnvManager caseSensitive', () => {
+	const em = EnvManager.caseSensitive();
+
+	em.set('a', '1');
+	em.set('A', '3');
+	em.set('b', '2');
+
+	assertEquals(em.get('a'), '1');
+	assertEquals(em.get('A'), '3');
+	assertEquals(em.get('b'), '2');
+	assertEquals(em.get('c'), '');
+});
+
+Deno.test('EnvManager caseInsensitive', () => {
+	const em = EnvManager.caseInsensitive();
+
+	em.set('a', '1');
+	em.set('A', '3');
+	em.set('b', '2');
+
+	assertEquals(em.get('a'), '3');
+	assertEquals(em.get('A'), '3');
+	assertEquals(em.get('b'), '2');
+	assertEquals(em.get('c'), '');
+});
+
+Deno.test('EnvManager parse', () => {
+	const em = EnvManager.caseInsensitive();
+
+	em.set('name', 'Steve');
+	em.set('greet', em.expand('Hello, $NAME'));
+
+	assertEquals(em.expand('$NAME'), 'Steve');
+	assertEquals(em.expand('${NAME}'), 'Steve');
+	assertEquals(em.expand('%NAME%'), 'Steve');
+	assertEquals(em.get('greet'), 'Hello, Steve');
+});
