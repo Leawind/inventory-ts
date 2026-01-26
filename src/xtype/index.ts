d39a05a487af1a67196e5ae57948ae6ba4a75018
Leawind <leawind@yeah.net>
2026-01-26 17:50:17 +0800

    style(xtype): add doc

--- src/xtype/index.ts
+++ src/xtype/index.ts
@@ -1,9 +1,41 @@
+/**
+ * ### Example
+ *
+ * ```ts
+ * type A = { a: string, b: number };
+ * type B = { a: number, c: boolean };
+ *
+ * type Result = Intersect<[A, B]>;
+ * type Result = { a: never, b: number, c: boolean }
+ * ```
+ */
 export type Intersect<T extends unknown[]> = T extends [infer First, ...infer Rest] ? First & Intersect<Rest>
 	: unknown;
-
+/**
+ * ### Example
+ *
+ * ```ts
+ * type A = { a: string, b: number };
+ * type B = { a: number, c: boolean };
+ *
+ * type Result = Union<[A, B]>;
+ * type Result = { a: string | number }
+ * ```
+ */
 export type Union<T extends unknown[]> = T extends [infer First, ...infer Rest] ? First | Union<Rest>
 	: never;
 
+/**
+ * ### Example
+ *
+ * ```ts
+ * type A = { a: string, b: number };
+ * type B = { a: number, c: boolean };
+ *
+ * type Result = MergeNoConflict<[A, B]>;
+ * type Result = { b: number, c: boolean }
+ * ```
+ */
 export type DiffProps<T extends unknown[]> = Omit<Intersect<T>, keyof Union<T>>;
 
 export function xorMerge<T extends object[]>(...objs: T): DiffProps<T> {
