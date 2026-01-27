645e83d3568d96e35681a3c5238b6340bd700bec
Leawind <leawind@yeah.net>
2026-01-27 11:27:22 +0800

    add(preview)

---
+++ src/preview/index.ts
@@ -0,0 +1,23 @@
+export function openURI(uri: string): Deno.Command {
+	let cmd: string;
+	let args: string[];
+
+	switch (Deno.build.os) {
+		case 'windows':
+			cmd = 'powershell';
+			args = ['-NoProfile', '-Command', `Start-Process "${uri}"`];
+			break;
+		case 'darwin':
+			cmd = 'open';
+			args = [uri];
+			break;
+		case 'linux':
+			cmd = 'xdg-open';
+			args = [uri];
+			break;
+		default:
+			throw new Error('Unsupported OS');
+	}
+
+	return new Deno.Command(cmd, { args });
+}
