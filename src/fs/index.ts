8dbe980fff603531baa079466701551a8f59f44b
Leawind <leawind@yeah.net>
2026-01-29 21:28:49 +0800

    refactor(fs): simplify module exports using export *

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,24 +1,6 @@
-import * as xtype from '../xtype/index.ts';
-
-import * as basic from './basic.ts';
-import * as operate from './operate.ts';
-import * as path from './path.ts';
-import * as temp from './temp.ts';
-import * as utils from './utils.ts';
-import * as walk from './walk.ts';
-
-export default xtype.xorMerge(
-	basic,
-	operate,
-	path,
-	temp,
-	utils,
-	walk,
-) as xtype.DiffProps<[
-	typeof basic,
-	typeof operate,
-	typeof path,
-	typeof temp,
-	typeof utils,
-	typeof walk,
-]>;
+export * from './basic.ts';
+export * from './operate.ts';
+export * from './path.ts';
+export * from './temp.ts';
+export * from './utils.ts';
+export * from './walk.ts';
