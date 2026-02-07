989027fa514a5c3a983874f4a5273030d56688e7
Leawind <leawind@yeah.net>
2026-02-07 22:59:50 +0800

    refactor(types): move type definitions to alias module

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,3 @@
-export type AnyFunction<R = any, Params extends any[] = any[]> = (...args: Params) => R
-
-export type Constructor<Inst = any, Params extends any[] = any[]> = new (...args: Params) => Inst
-
-export type Awaitable<T> = Promise<T> | T
-
 // Index start >>>>>>>>>>>>>>>>
 export * from './alias.ts'
 export * from './non-distributive.ts'
