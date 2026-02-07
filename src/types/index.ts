e6571e848d96c96160ba740b2e5e7529ee35c2b8
Leawind <leawind@yeah.net>
2026-02-07 23:42:07 +0800

    feat(types): add boolean type export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 // Index start >>>>>>>>>>>>>>>>
 export * from './alias.ts'
+export * from './boolean.ts'
 export * from './non-distributive.ts'
 export * from './object.ts'
 export * from './set.ts'
