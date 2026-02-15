abb461cfb839c73a3d1b48815e4106a1161f10f5
Leawind <leawind@yeah.net>
2026-02-15 12:31:49 +0800

    test(types): disable unstable union test cases

--- test/types/union.test.ts
+++ test/types/union.test.ts
@@ -16,11 +16,11 @@ type C = { b: number; c: boolean }
   expect<UnionToTuple<any>>().to.be<[any]>().pass
   expect<UnionToTuple<unknown>>().to.be<[unknown]>().pass
 
-  expect<UnionToTuple<3 | 4 | 5>>().to.be<[3, 4, 5]>().pass
-  expect<UnionToTuple<string | number>>().to.be<[string, number]>().pass
-  expect<UnionToTuple<number | string>>().to.be<[string, number]>().pass
+  // expect<UnionToTuple<3 | 4 | 5>>().to.be<[3, 4, 5]>().pass
+  // expect<UnionToTuple<string | number>>().to.be<[string, number]>().pass
+  // expect<UnionToTuple<number | string>>().to.be<[string, number]>().pass
 
-  expect<UnionToTuple<A | B | C>>().to.be<[A, B, C]>().pass
-  expect<UnionToTuple<C | B | A>>().to.be<[A, B, C]>().pass
+  // expect<UnionToTuple<A | B | C>>().to.be<[A, B, C]>().pass
+  // expect<UnionToTuple<C | B | A>>().to.be<[A, B, C]>().pass
   expect<UnionToTuple<A | A>>().to.be<[A]>().pass
 }
